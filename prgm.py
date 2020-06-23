114bc624db0c2916b74b3ebb82d37b74f258a579
dharsika09 <67306796+dharsika09@users.noreply.github.com>
2020-06-23 13:37:58 +0530

    prgm.py
    to initialize the values,to select the value in a tuple and to delete a value in a dictionary

---
+++ prgm.py
@@ -0,0 +1,17 @@
+list1=['mat','pant','hat']
+list2=['fan','key','rule']
+tuple1=('h','e','l','l','o')
+dict1={
+        "name"="john",
+        "class"=12,
+        "group"="biomaths"
+       }
+list1.append('rat')
+list2.append('chair')
+print(list1)
+print(list2)
+del dict["class"]
+print(dict)
+print(tuple1[0])
+print(tuple1[3])
+       
